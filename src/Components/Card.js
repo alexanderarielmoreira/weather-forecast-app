2c1ea01135ae8fa3059604158f809f3781b0ca73
Ariel <alarmoreira@gmail.com>
2023-03-20 19:54:30 -0300

    Se ultiman detalles para mostrar la información.

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -17,6 +17,14 @@ const Card = ({
     let url = ''; 
     let iconUrl = '';
 
+    let iconUrl3 = '';
+    let iconUrl6 = '';
+    let iconUrl9 = ''; 
+
+    let forecastDate3 = '';
+    let forecastDate6 = '';
+    let forecastDate9 = '';
+    
     if(loadingData){
         return <Spinner />; 
     }
@@ -24,29 +32,66 @@ const Card = ({
     if(showData){
         url = 'http://openweathermap.org/img/w/'; 
         iconUrl = url + weather.weather[0].icon + '.png'; 
+
+        iconUrl3 = url + forecast.list[1].weather[0].icon + '.png';
+        iconUrl6 = url + forecast.list[2].weather[0].icon + '.png';
+        iconUrl9 = url + forecast.list[3].weather[0].icon + '.png'; 
+
+        forecastDate3 = forecast.list[1].dt_txt.substring(8, 10) + '/' + forecast.list[1].dt_txt.substring(5, 7) + '/' + forecast.list[1].dt_txt.substring(0, 4) + ' ' + forecast.list[1].dt_txt.substring(11, 13);
+        
+        forecastDate6 = forecast.list[2].dt_txt.substring(8, 10) + '/' + forecast.list[2].dt_txt.substring(5, 7) + '/' + forecast.list[2].dt_txt.substring(0, 4) + ' ' + forecast.list[2].dt_txt.substring(11, 13);
+
+        forecastDate9 = forecast.list[3].dt_txt.substring(8, 10) + '/' + forecast.list[3].dt_txt.substring(5, 7) + '/' + forecast.list[3].dt_txt.substring(0, 4) + ' ' + forecast.list[3].dt_txt.substring(11, 13); 
     }
     
     return(
-            <div className="mt-5">
+            <div className="mt-5"> 
                 {
                     showData === true ? (
                         <div className="container"> 
                             <div className="card mb-3 msx-auto bg-dark text-light"> 
                                 <div className="row g-0">
                                     <div className="col-md-4">
-                                    <h4 className="card-title "> {weather.name} </h4>
+                                    <h4 className="card-title"> {weather.name} </h4>
                                     <p className="card-date"> {date} </p> 
                                     <h3 className="card-temp"> {(weather.main.temp - 273.15).toFixed(1)}° C </h3>
                                     <p className="card-desc"> <img src={iconUrl} alt="icon-weather" /> {weather.weather[0].description} </p> 
-                                    <img src='https://images.pexels.com/photos/2434268/pexels-photo-2434268.jpeg?auto=compress&cs=tinysrgb&w=600' 
-                                    className="img-fluid rounded-start"
+                                    <img src='https://images.pexels.com/photos/2348811/pexels-photo-2348811.jpeg?auto=compress&cs=tinysrgb&w=600' 
+                                    className="img-fluid rounded-start" 
                                     alt="background-image"
                                     />
                                     </div>
 
                                     <div className="col-md-8">
-                                        <div className="card-body text-start mt-2">
+                                        <div className="card-body msx-auto mt-2">
+                                              <h5 className="card-text">Velocidad del viento: {(weather.wind.speed)} m/s</h5>
+                                              <h5 className="card-text">Temperatura máxima: {(weather.main.temp_max - 273.15).toFixed(1)}° C</h5>
+                                              <h5 className="card-text">Temperatura mínima: {(weather.main.temp_min - 273.15).toFixed(1)}° C</h5>
+                                              <h5 className="card-text">Sensación térmica: {(weather.main.feels_like - 273.15).toFixed(1)}° C</h5>
+                                              <h5 className="card-text">Humedad: {(weather.main.humidity)} %</h5>
                                               
+                                        </div>
+                                        <hr />
+
+                                        <div className="row mt-4">
+                                            <div className="col">
+                                                <p> {forecastDate3} horas</p>
+                                                <p className="description"> <img src={iconUrl3} alt="icon-3" /> {forecast.list[1].weather[0].description} </p> 
+                                                <p className="temp"> {(forecast.list[1].main.temp - 273.15).toFixed(1)}° C</p>
+                                            </div>
+
+                                            <div className="col">
+                                                <p> {forecastDate6} horas</p>
+                                                <p className="description"> <img src={iconUrl6} alt="icon-6" /> {forecast.list[2].weather[0].description} </p> 
+                                                <p className="temp"> {(forecast.list[2].main.temp - 273.15).toFixed(1)}° C</p> 
+                                            </div>
+
+                                            <div className="col">
+                                                <p> {forecastDate9} horas</p>
+                                                <p className="description"> <img src={iconUrl9} alt="icon-9" /> {forecast.list[3].weather[0].description} </p> 
+                                                <p className="temp"> {(forecast.list[3].main.temp - 273.15).toFixed(1)}° C</p> 
+                                            </div>
+
                                         </div>
 
                                     </div>
@@ -56,7 +101,7 @@ const Card = ({
                             
                         </div>
                     ):(
-                        <h2 className='text-light'>Sin datos</h2>
+                        <p className='text-light'>Sin datos para visualizar. Ingresa el nombre de una ciudad.</p>
                     )
 
                 }
